93861ebe5bf407ea292c7d13328435ac1a7c0074
LevMitkov <114360875+LevMitkov@users.noreply.github.com>
2022-11-02 21:29:31 +0300

    Update PZ_3_2.py

--- PZ_3/PZ_3_2.py
+++ PZ_3/PZ_3_2.py
@@ -3,11 +3,14 @@ try:
     b = float(input())
     c = float(input())
     if (a > b) and (b > c):
-        print(a*2, b*2, c*2)
-    else:
         a = -a
         b = -b
         c = -c
+        print(a, b, c)
+    else:
+        a = a * 2
+        b = b * 2
+        c = c * 2
         print(a, b, c)
 except:
     print('Ошибка')
