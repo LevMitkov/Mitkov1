ae0ab105eb7e200f4903f1e3b9d5a154401db415
LevMitkov <114360875+LevMitkov@users.noreply.github.com>
2022-11-12 00:31:13 +0300

    Update PZ_3_2.py

--- PZ_3/PZ_3_2.py
+++ PZ_3/PZ_3_2.py
@@ -1,14 +1,14 @@
 try:
-    a = float(input())
+    a = float(input())      # Ввод значений переменных
     b = float(input())
     c = float(input())
-    if (a > b) and (b > c):
-        a = -a
+    if (a > b) or (b > c): # Проверка на упорядоченность по возрастанию
+        a = -a             # Присваиваем противоположные значения
         b = -b
         c = -c
         print(a, b, c)
     else:
-        a = a * 2
+        a = a * 2          # Присваиваем значения в 2 раза больше
         b = b * 2
         c = c * 2
         print(a, b, c)
